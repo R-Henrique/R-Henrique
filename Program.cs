723d7f615a8796b4392f653409f8f0c654973765
Ryan Henrique de Aguiar dos Santos <gtsupera12@gmail.com>
2022-04-05 17:58:23 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Quadrado q1 = new Quadrado(10.0);
+Quadrado q2 = new Quadrado(5.5);
+
+Console.WriteLine(q1.Lado);
+Console.WriteLine(q2.Lado);
+
+q1.Lado = -5.0;
+Console.WriteLine(q1.Lado);
+
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+Console.WriteLine(q2.Area);
+Console.WriteLine(q2.Perimetro);
+
